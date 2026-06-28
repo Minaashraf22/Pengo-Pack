cf63d71760327edd2d4d592b3998cc1499fb36ab
Mina Ashraf <minaashraf1517@gmail.com>
2026-06-28 15:11:58 +0300

    Add Vercel Analytics & Speed Insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react';
+
 import App from './App.tsx'
 
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
+
 createRoot(document.getElementById('root')!).render(
- <StrictMode>
-  <App />
-  <Analytics />
-</StrictMode>
+  <StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </StrictMode>
 )
